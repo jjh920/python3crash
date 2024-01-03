e6b604144234552bd708fbc3011fcc45dede3661
JuHyeon, Cho <jjh920@naver.com>
2024-01-03 18:18:01 +0900

    사원등록 프로그램 V2 - json으로 처리

---
+++ 34employees.py
@@ -0,0 +1,18 @@
+# 사원정보를 입력받아 employees.json 파일에 저장
+# employees.csv 파일에 저장된 사원정보를 화면 출력
+# 저장시 : 사번, 이름, 성, 이메일, 입사일, 직책, 급여, 부서번호
+# 출력시 ; 사번, 이름, 직책, 부서번호
+import joju.empv2 as emp
+
+emp.load_employees()
+
+while True:
+    menu = emp.show_menu()
+
+    if menu == '1': emp.add_employee()                          #C
+    elif menu == '2': emp.read_employee()                       #R
+    elif menu == '3': emp.readone_employee()                    #R
+    elif menu == '4': emp.modify_employee()                     #U
+    elif menu == '5': emp.remove_employee()                     #D
+    elif menu == '0': emp.exit_program()
+    else: print('메뉴를 잘못 선택하셨습니다!!')
