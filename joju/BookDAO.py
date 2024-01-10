28a2a59c6ef9261c6bd6548057ff557d63dd1bab
JuHyeon, Cho <jjh920@naver.com>
2024-01-10 14:50:44 +0900

    도서관리 프로그램 v1 -selectone_book 구현

--- joju/BookDAO.py
+++ joju/BookDAO.py
@@ -3,9 +3,9 @@ import joju.dbinfo2 as dbinfo
 insertsql = ' insert into book (bkname, author, publisher, pubdate, retail, '\
             ' price, pctoff, mileage) values (%s, %s, %s, %s, %s, %s, %s, %s) '
 selectsql = 'select bkno, bkname, author, publisher, price from book'
-selectonesql = 'select * from book where bkno = 1'
+selectonesql = 'select * from book where bkname = %s'
 updatesql = 'update book set bkname = %s, author = %s, publisher = %s, ' \
-            'pubdate = %s, retail = -1, pctoff = -1'
+            'pubdate = %s, retail = %s, pctoff = %s'
 deletesql = 'delete from book where bkno = ?'
 
 class BookDAO:
@@ -34,8 +34,14 @@ class BookDAO:
         return rows
 
     @staticmethod
-    def selectone_book():
-        pass
+    def selectone_book(bkname):
+        cursor, conn = dbinfo.openConn()
+
+        cursor.execute(selectonesql, [bkname])
+        row = cursor.fetchone()
+
+        dbinfo.closeConn(cursor, conn)
+        return row
 
     @staticmethod
     def update_book():
